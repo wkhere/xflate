fd9c3cb915f4ef78a828198d5c8df5494879fce1
Wojciech Kaczmarek <wojtekk@kofeina.net>
2019-02-04 15:58:38 +0100

    parse flags even though no flags are expected

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"compress/flate"
+	"flag"
 	"io"
 	"log"
 	"os"
@@ -9,7 +10,16 @@ import (
 
 func init() { log.SetFlags(0) }
 
+func parseFlags() {
+	flag.Usage = func() {
+		log.Println("Usage: deflate\t\t(reads stdin, outputs to stdout)")
+	}
+	flag.Parse()
+}
+
 func main() {
+	parseFlags()
+
 	_, err := io.Copy(os.Stdout, flate.NewReader(os.Stdin))
 	if err != nil {
 		log.Fatal(err)
